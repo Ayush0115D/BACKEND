69b14f80357d3b5f0306598a6fd6fc0c44172a97
Ayush0115D <dhakreayush578@gmail.com>
2025-06-22 23:54:27 +0530

    learned about modules

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log("hey there js");
+console.log("hey there! i am js");
+const math=require("./math") //for import-require fucn
+console.log("math value is ",math.addFn(2,4))
+
+// console.log("hey there! i am js");
+// const math=require("./math") 
+// console.log("math value is ",math)
---
+++ math.js
@@ -0,0 +1,13 @@
+function add (a,b ){
+    return a+b;
+}
+function sub(a,b ){
+    return a-b;
+}
+module.exports={
+    addFn:add,
+    subFn:sub,
+} //better way
+
+// exports.add1=(a,b)=>a+b;
+// exports.sub2=(a,b)=>a-b;
