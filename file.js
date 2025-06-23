4af59a15139513a79cf0814547145c1a1688869a
Ayush0115D <dhakreayush578@gmail.com>
2025-06-23 23:00:07 +0530

    learned file handling

---
+++ file.js
@@ -0,0 +1,23 @@
+const fs=require("fs");
+//sync 
+// fs.writeFileSync("./test.txt","helloworld");
+//async
+// fs.writeFile("./test.txt","helloworld",(err)=>{});
+
+// const result= fs.readFileSync("./contact.txt","utf-8") // standard encoding
+// console.log(result);
+
+// fs.readFile("./contact.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("error",err);
+//     } else {
+//         console.log(result);
+//     }
+// });
+
+fs.appendFileSync("./test.txt",`${Date.now()}hey there\n`);
+
+//  fs.cpSync("./test.txt","./copy.txt")
+//  fs.unlinkSync(",.test.txt") //delete
+console.log(fs.statSync("./copy.txt"));
+fs.mkdirSync("my-docs/a/b",{recursive:true});//makedirectory
