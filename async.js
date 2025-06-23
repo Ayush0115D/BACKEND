d04b852d674384c4aa8c1ef6d7a1012ed6927370
Ayush0115D <dhakreayush578@gmail.com>
2025-06-24 00:07:27 +0530

    few updations done

--- async.js
+++ async.js
@@ -1,6 +1,9 @@
 const fs=require("fs");
+const os=require("os")
+console.log(os.cpus().length)
 console.log("1")
-//non blocking
+
+//non blocking opern..ye hume async result deta
 fs.readFile("./contact.txt","utf-8",(err,result)=>{
     console.log(result)
 });
