8319ab934ad0232ffbe82e89e52fe1c2ea88292e
Ayush0115D <dhakreayush578@gmail.com>
2025-06-24 23:57:32 +0530

    leared url handling

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const http=require("http")
 const fs=require("fs")
+const url=require("url")
 const myServer=http.createServer ((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
+     
     const log=`${Date.now()} :${req.url } new request received \n`;
+    const myUrl=url.parse(req.url,true)
+    console.log(myUrl)
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case'/':res.end("homepage");
             break
-            case'/about':res.end("i am piyush garg");
+            // case'/about':res.end("i am piyush garg");
+            case '/about':
+                const username=myUrl.query.myname;
+                res.end(`Hi,${username}`);
             break
+            case 'search'://yt do like this
+            const search=myUrl.query.search_query;
+            res.end("here are your results for"+search)
+            break;
+
             default:res.end("404 not found ")
         }
     // res.end("Hello from server again")
