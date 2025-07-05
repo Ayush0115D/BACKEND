ee597ab1465d77d721f0df67653a14a31f39f0dc
Ayush0115D <dhakreayush578@gmail.com>
2025-07-05 23:46:10 +0530

    MVC

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,9 +1,10 @@
-
-const users=require("./MOCK_DATA.json")
+// const users=require("./MOCK_DATA.json");
+const express=require("express");
+const connectMongoDb=require("./routes/user");
+const {logReqRes}=require("./middlewares");
+const userRouter=require("./routes/user");
 const app=express();
 const port=8000;
-const userRouter=require("./routes/user");
-const connectMongoDb=require("./routes/user")
 
 //MONGOOSE CONNECTION
 // mongoose
@@ -16,20 +17,19 @@ connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1")
 
 //middleware-just like a plugin
 app.use(express.urlencoded({extended:false}));
-
-app.use((req,res,next)=>{
-   console.log("hello from middleware 1");
-   req.myUserName="ayushdhakre.dev";
-//    return res.json({mgs:"hello from middleware1"});
-next();
-})
-app.use((req,res,next)=>{
-   console.log("hello from middleware 2",req.myUserName);
-// return res.end("hey")
-next();
-})
+// app.use((req,res,next)=>{
+//    console.log("hello from middleware 1");
+//    req.myUserName="ayushdhakre.dev";
+// //    return res.json({mgs:"hello from middleware1"});
+// next();
+// })
+// app.use((req,res,next)=>{
+//    console.log("hello from middleware 2",req.myUserName);
+// // return res.end("hey")
+// next();
+// })
+app.use(logReqRes("log.txt"));
 //Routes
-
 // app.patch("/api/users/:id",(req,res)=>{
 //     //todo:edit  user with id
 //     return res.json({status:"pending"})
