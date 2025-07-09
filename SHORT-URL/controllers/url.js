410eceeacd5545afc746b1f1aa7ec067fa4b1214
Ayush0115D <dhakreayush578@gmail.com>
2025-07-09 23:59:20 +0530

    ui updated

--- SHORT-URL/controllers/url.js
+++ SHORT-URL/controllers/url.js
@@ -5,11 +5,11 @@ const body = req.body;
 if (!body.url)  return res.status(400).json({ error: "URL is required" });
 const shortId = nanoid(10);
 await URL.create({
-shortd: shortId,   
+shortid: shortId,   
 redirectUrl: body.url, 
 visitHistory: [], 
 });
-return res.status(200).json({id: shortId});
+return res.render("home", { id: shortId });
 }
 async function handleGetAnalytics(req, res) {
 const shortId = req.params.shortId;
--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -1,15 +1,23 @@
 const express=require("express");
+const path=require("path");
 const urlRouter=require("./routes/url");
 const {connectToMongoDB}=require("./connect");
 const app=express();
+
 const URL = require("./models/url");
+const staticRouter = require("./routes/StaticRouter");
 const PORT=8001;
+
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(() => console.log("Connected to MongoDB"))
 app.set("view engine", "ejs");
-app.set("views", path.resolve ("/views"));
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/url", urlRouter);
+app.use("/", staticRouter);
+
 app.get("/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
   const entry= await URL.findOneAndUpdate(
