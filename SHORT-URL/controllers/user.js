c22e185e258d0292eb721ea98b2357067fd4c4c8
Ayush0115D <dhakreayush578@gmail.com>
2025-07-10 21:55:45 +0530

    added uuid package and cookies

--- SHORT-URL/controllers/user.js
+++ SHORT-URL/controllers/user.js
@@ -1,4 +1,6 @@
+const{v4:uuidv4} = require('uuid');
 const User=require('../models/user');
+const{setUser} = require('../service/auth');
 async function handleUserSignup(req,res){
     const { name, email, password } = req.body;
     await User.create({ name, email, password });
@@ -10,9 +12,13 @@ async function handleUserLogin(req, res) {
     if (!user) {
         return res.render("login",{error:"Invalid email or password",});
     }
-    return res.render("home", { urls: [] } ); // Render home page after successful login :)
+    const sessionId = uuidv4();  
+    setUser(sessionId, user);
+    res.cookie("uid", sessionId);
+    return res.redirect("/", ); // Render home page after successful login :)
 }   
 module.exports = {
     handleUserSignup,
     handleUserLogin
 };
+ 
