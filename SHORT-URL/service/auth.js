d0d179bb119a4238829aaece99a5f48a0c06a17d
Ayush0115D <dhakreayush578@gmail.com>
2025-07-11 22:05:54 +0530

    add jwt lib.

--- SHORT-URL/service/auth.js
+++ SHORT-URL/service/auth.js
@@ -1,6 +1,12 @@
+const jwt = require('jsonwebtoken');\
+const secret="ayushd1234";
 const sessionIdToUserMap = new Map(); //jb bhi server ko refresh krege usermap khali ho jayega
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser( user) {
+    const payload = {
+        id,
+         ...user,
+}
+return jwt.sign(payload,secret)
 }
 function getUser(id) { 
     return sessionIdToUserMap.get(id);
