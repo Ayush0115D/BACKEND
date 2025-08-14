5ca5aa8b930010fc77fb8cfd158a0ee70f136755
Ayush0115D <dhakreayush578@gmail.com>
2025-08-14 17:59:19 +0530

    websocket demonstration

--- Websockets/index.js
+++ Websockets/index.js
@@ -1,9 +1,21 @@
 const http=require("http")
 const express=require("express");
 const path=require("path");
+const {Server}=require("socket.io")
 
 const app=express();
 const server=http.createServer(app);
+const io=new Server(server);//handle sokets,express handle all htpp req.
+//socket.io
+io.on("connection", (socket) => {
+  socket.on("user message", (message) => {
+    console.log("Received user message:", message);
+    // Broadcast to all connected clients (including sender)
+    io.emit("chat message", message);
+  });
+});
+
+
 app.use(express.static((path.resolve("./public"))));
 app.get("/",(req,res)=>{
   res.sendFile(path.resolve("./public/index.html"));
